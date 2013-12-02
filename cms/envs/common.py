d91768c1aac61d1d073cfb25562730932569e59c
Cristian Salamea <ovnicraft@gmail.com>
2013-12-02 22:23:41 +0000

    [FIX] agregado sentry conf

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -562,3 +562,11 @@ for app_name in OPTIONAL_APPS:
 
 FEATURES['ENABLE_CREATOR_GROUP'] = True
 
+#sentry config
+RAVEN_CONFIG = {
+    'dsn': 'https://59734eda618a43afa2179d781549edeb:311de1f2e01247f284f3f72fbcd5844f@sentry.iaen.edu.ec/3',
+}
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
