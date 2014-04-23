d7abd228e0a58e8ffdeb232b9d96c20344409062
Cristian Salamea <ovnicraft@gmail.com>
2014-04-23 10:37:27 -0500

    allowed hosts CMS

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -624,7 +624,8 @@ ALLOWED_HOSTS = [
     'beta.cms.iaen.edu.ec',
     'beta.evex.iaen.edu.ec',
     'upex.iaen.edu.ec',
-    'cms.iaen.edu.ec'
+    'cms.iaen.edu.ec',
+    '192.168.20.87'
     ]
 
 FEATURES['ENABLE_CREATOR_GROUP'] = True
